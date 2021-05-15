fed2633ea1c90f02d3a0cc72c06456ae37d01fd5
liheng <liheng@pand.ai>
2021-05-15 19:33:28 +0800

    refactored code to separate zookeeper logic and main app

---
+++ src/main/java/Application.java
@@ -0,0 +1,60 @@
+import cluster.management.LeaderElection;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class Application implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        Application application = new Application();
+        ZooKeeper zooKeeper = application.connectToZookeeper();
+
+        LeaderElection leaderElection = new LeaderElection(zooKeeper);
+        leaderElection.volunteerForLeadership();
+        leaderElection.reelectLeader();
+
+        application.run();
+        application.close();
+        System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public ZooKeeper connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+        return zooKeeper;
+    }
+
+    public void run() throws InterruptedException {
+        // put main thread into a wait state, because connection is on another thread
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zooKeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+//        will be called by zookeeper on a separate thread whenever there is a new event coming from zookeeper server
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    //lose connection to zookeeper, we want to wake up main thread and allow application to close resources and exit
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+                break;
+        }
+    }
+}
---
+++ src/main/java/cluster/management/LeaderElection.java
@@ -0,0 +1,65 @@
+package cluster.management;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class LeaderElection implements Watcher {
+    private final ZooKeeper zooKeeper;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
+
+    public LeaderElection(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
+    }
+
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        // disconnect from zookeeper, the node will be deleted
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void reelectLeader() throws InterruptedException, KeeperException {
+        Stat predecessorStat = null;
+        String predecessorZnodeName = "";
+
+        while (predecessorStat == null) {
+            List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+
+            // find which is smallest(earliest)
+            Collections.sort(children);
+            String smallestChild = children.get(0);
+            if (smallestChild.equals(currentZnodeName)) {
+                System.out.println("I am the leader");
+                return;
+            } else {
+                System.out.println("I am not the leader, " + smallestChild + " is the leader");
+                int predecessorIndex = Collections.binarySearch(children, currentZnodeName) - 1;
+                predecessorZnodeName = children.get(predecessorIndex);
+                predecessorStat = zooKeeper.exists(ELECTION_NAMESPACE + '/' + predecessorZnodeName, this);
+            }
+            System.out.println("Watching znode " + predecessorZnodeName);
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+//        will be called by zookeeper on a separate thread whenever there is a new event coming from zookeeper server
+        switch (event.getType()) {
+            case NodeDeleted:
+                try {
+                    reelectLeader();
+                } catch (InterruptedException | KeeperException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+}
